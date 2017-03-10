6902e148edf60b233c58d4836e7f475208018ead
Elentary <amareelez@gmail.com>
2017-03-11 01:28:03 +0300

    Task 19.2 77/100 O(n^2*m)
    Signed-off-by: Elentary <amareelez@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,47 +1,63 @@
 #include <vector>
+#include <set>
 
 using namespace std;
 
 #ifdef ONLINE_JUDGE
 
 #include <fstream>
-#include <map>
 
-ifstream cin("input.txt");
-ofstream cout("output.txt");
+ifstream cin("concert.in");
+ofstream cout("concert.out");
 #else
 #include <iostream>
 #endif
-
-vector<vector<long long> > dp;
-vector<pair<int, int> > dims;
-
-long long calc(int i, int j) {
-    if (dp[i][j] != LLONG_MAX)
-        return dp[i][j];
-
-    long long ans = LLONG_MAX;
-    for (int k = i; k < j; k++) {
-        ans = min(ans, calc(i, k) + calc(k + 1, j) + dims[i].first * dims[k].second * dims[j].second);
-    }
-    dp[i][j] = ans;
-    return ans;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    cin >> n;
-    dims.resize(n);
+    int n, m;
+    long long d;
+    cin >> n >> m >> d;
+    vector<long long> songs(n);
     for (int i = 0; i < n; i++)
-        cin >> dims[i].first >> dims[i].second;
+        cin >> songs[i];
 
-    dp.resize(n, vector<long long>(n, LLONG_MAX));
+    vector<vector<long long> > v(n, vector<long long>(n, 0));
+    for (int i = n - 1; i >= 0; i--) {
+        multiset<long long, greater<long long> > s;
+        long long currTime = 0;
+
+        for (int j = i; j < n; j++) {
+            if (currTime + songs[j] <= d) {
+                s.insert(songs[j]);
+                currTime += songs[j];
+            } else if (!s.empty() && *s.begin() > songs[j]) {
+                currTime -= *s.begin() - songs[j];
+                s.erase(s.begin());
+                s.insert(songs[j]);
+            }
+            v[i][j] = s.size();
+        }
+    }
+
+    vector<vector<long long> > dp(n, vector<long long>(m + 1, 0));
     for (int i = 0; i < n; i++)
-        dp[i][i] = 0;
+        dp[i][1] = v[0][i];
 
-    cout << calc(0, n - 1) << "\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 2; j <= m; j++) {
+            long long tempAns = 0;
+            for (int k = 0; k < i; k++) {
+                tempAns = max(tempAns, dp[k][j - 1] + v[k + 1][i]);
+            }
+
+            dp[i][j] = max(v[0][i], tempAns);
+        }
+    }
+
+    cout << dp[n - 1][m] << "\n";
+
     return 0;
 }
